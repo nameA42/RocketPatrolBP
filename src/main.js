f1e5976fc7ec67b5bf60f336d456d697d91791cf
nameA42 <bepratt@ucsc.edu>
2022-04-21 09:27:53 -0700

    point breakdown

--- src/main.js
+++ src/main.js
@@ -22,3 +22,32 @@ let borderOver = 500;
 // reserve keyboard vars
 let keyUP, keyR, keyLEFT, keyRIGHT, keyA, keyS, keyD;
 
+/*
+Point Breakdown
+
+PvP collision (+5) - 5
+    [implementing arcade took up most of my development time]
+
+Random Ship Direction (+5) - 10
+
+New Background (+5) - 15
+
+Control after Fire (+5) - 20
+
+Time Display (+10) - 30
+
+Zoom and Flash (+10) - 40
+    [the zoom and flashing screen on death takes about as much time and research as the Time display]
+
+New Border UI (+10) - 50
+    [extended UI and add border for split screen/zoom compatibility]
+
+New Assets (+20) - 70
+
+Split Screen (+20) - 90
+    [with seperate text and camera displays, I think this is about a +20]
+
+Simultanious 2 player (+30) - 120
+
+TOTAL: 120
+*/
